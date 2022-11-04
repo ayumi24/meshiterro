fe2bd88037f270782f3e9370b245ae40f8806b9c
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-11-04 11:01:20 +0000

    削除機能追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,6 +19,13 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
   end
 
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/post_images'
+
+  end
+
   private
 
   def post_image_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  root to: "homes#top"
  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
 
  get 'homes/about' => "homes#about", as: 'about'
