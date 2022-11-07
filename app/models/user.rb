a39ae50e206268df94f0aeb05590b1f5f0923190
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2022-11-07 13:04:23 +0000

    部分テンプレート

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
   has_many :post_images,dependent: :destroy
   has_many :post_comments,dependent: :destroy
-  has_one_attached :profile_image
+  has_many :favorites,dependent: :destroy
+
 
   def get_profile_image(width, height)
     unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorites, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
@@ -11,5 +12,3 @@ Rails.application.routes.draw do
 
  get 'homes/about' => "homes#about", as: 'about'
 end
-
-
